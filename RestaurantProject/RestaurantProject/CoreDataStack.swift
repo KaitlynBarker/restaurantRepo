9af847a1ab9cf170f2abc6780a26b5b50882605f
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2017-09-12 10:44:36 -0600

    add core data stack and start model objects

---
+++ RestaurantProject/RestaurantProject/CoreDataStack.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataStack.swift
+//  RestaurantProject
+//
+//  Created by Kaitlyn Barker on 9/12/17.
+//  Copyright © 2017 Kaitlyn Barker. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "RestaurantProject")
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+}
