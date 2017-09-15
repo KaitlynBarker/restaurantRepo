7b131deda08838b7715b12157cd259d7831166fd
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MacBook-Pro.local>
2017-09-15 16:02:16 -0600

    add the favorite and dislike view controllers on the story board and starting the code for that

---
+++ RestaurantProject/RestaurantProject/DislikeListTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DislikeListTableViewController.swift
+//  RestaurantProject
+//
+//  Created by Kaitlyn Barker on 9/15/17.
+//  Copyright © 2017 Kaitlyn Barker. All rights reserved.
+//
+
+import UIKit
+
+class DislikeListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DislikeCell", for: indexPath)
+        
+        
+        
+        return cell
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
---
+++ RestaurantProject/RestaurantProject/FavoritesListTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FavoritesListTableViewController.swift
+//  RestaurantProject
+//
+//  Created by Kaitlyn Barker on 9/15/17.
+//  Copyright © 2017 Kaitlyn Barker. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
+
+        
+
+        return cell
+    }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
